2b71919a9d6958446f4ab65a79293de1a4d3af66
Woosung <127220113+ws19999@users.noreply.github.com>
2024-06-20 16:09:06 +0900

    [Gold IV] Title: 화장실의 규칙, Time: 100 ms, Memory: 72412 KB -BaekjoonHub

--- 백준/Gold/19640. 화장실의 규칙/화장실의 규칙.cc
+++ 백준/Gold/19640. 화장실의 규칙/화장실의 규칙.cc
@@ -3,30 +3,29 @@
 #include <queue>
 #include <tuple>
 using namespace std;
-int N, M,K;
-tuple<int, int, int,int> Deca;
+int N, M,K,Deca;
 deque<tuple<int, int,int,int>> dq[100000];
 priority_queue<tuple<int, int, int,int>> pq;
 int main(void)
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> N >> M >> K;
+	Deca = K;
 	for (int i = 0; i < N; i++)
 	{
 		int D, H;
 		cin >> D >> H;
 		dq[i % M].push_back(make_tuple(D, H,-(i%M),i));
 		if (i < M)pq.push(make_tuple(D, H, -(i%M),i));
-		if (i == K)Deca = make_tuple(D, H, -(i%M),i);
 	}
 	int p = 0;
 	while (true)
 	{
-		if (pq.top() == Deca)break;
+		if (get<3>(pq.top()) == Deca)break;
 		int pos = (-get<2>(pq.top())) % M;
+		int cnt = get<3>(pq.top());
 		pq.pop();
-		dq[pos].pop_front();
-		if (!dq[pos].empty())pq.push(dq[pos].front());
+		if (cnt+M<N)pq.push(dq[pos][cnt/M+1]);
 		p++;
 	}
 	cout << p;
