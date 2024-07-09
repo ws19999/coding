ba69c390cfb828cf1420aa39e939401927b92961
Woosung <127220113+ws19999@users.noreply.github.com>
2024-07-09 11:03:43 +0900

    [Gold IV] Title: 우체국, Time: 68 ms, Memory: 8360 KB -BaekjoonHub

---
+++ 백준/Gold/2141. 우체국/우체국.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <map>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N,answer=0;
+	cin >> N;
+	long long total = 0,temp=0;
+	map<int, long long> village;
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		long long a;
+		cin >> x >> a;
+		village[x] += a;
+		total += a;
+	}
+	long long mid = total / 2;
+	if (total % 2 == 1)mid++;
+	for (auto v : village)
+	{
+		temp += v.second;
+		answer = v.first;
+		if (temp >= mid)break;
+	}
+	cout << answer;
+	return 0;
+}
