a9c18b586b756abe1b06e1c8de41ee97781d5352
Woosung <127220113+ws19999@users.noreply.github.com>
2024-07-11 15:50:35 +0900

    [Silver II] Title: 지뢰, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.cc
+++ 백준/Silver/2232. 지뢰/지뢰.cc
@@ -1,23 +1,12 @@
 #include <iostream>
-#include <deque>
 using namespace std;
+int table[50001];
 int main(void)
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int N,answer=0;
 	cin >> N;
-	deque<int> dq;
-	dq.push_back(0);
-	for (int i = 0; i < N; i++)
-	{
-		int num;
-		cin >> num;
-		dq.push_back(num);
-	}
-	dq.push_back(0);
-	for (int i = 1; i <= N; i++)
-	{
-		if (dq[i] >= dq[i - 1] and dq[i] >= dq[i + 1])cout << i << "\n";
-	}
+	for (int i = 1; i <= N; i++)cin >> table[i];
+	for (int i = 1; i <= N; i++) if (table[i] >= table[i - 1] and table[i] >= table[i + 1])cout << i << "\n";
 	return 0;
 }
