cc81debe8c0e3ce7d7e9f03be5cb6ed735dede41
Woosung <127220113+ws19999@users.noreply.github.com>
2024-07-20 21:45:06 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,50 +1,22 @@
 #include <iostream>
 using namespace std;
-int map[17][17],N,ans=0;
-int dx[] = { 0,1,1 };
-int dy[] = { 1,1,0 };
-void dfs(int x,int y, int dir)
-{
-	if (x == N && y == N)
-	{
-		ans++;
-		return;
-	}
-	if (dir == 0)
-	{
-		if (y + 1 <= N && !map[x][y+1])
-		{
-			dfs(x, y + 1, 0);
-			if (x + 1 <= N && !map[x+1][y+1] && !map[x+1][y])dfs(x + 1, y + 1, 1);
-		}
-	}
-	else if (dir == 1)
-	{
-		if (y + 1 <= N && !map[x][y+1])dfs(x, y + 1, 0);
-		if (x + 1 <= N && !map[x+1][y])dfs(x + 1, y, 2);
-		if (x + 1 <= N && y + 1 <= N && !map[x][y+1] && !map[x+1][y] && !map[x+1][y+1])dfs(x + 1, y + 1, 1);
-	}
-	else if (dir == 2)
-	{
-		if (x + 1 <= N && !map[x+1][y])
-		{
-			dfs(x + 1, y, 2);
-			if (y + 1 <= N && !map[x][y+1] && !map[x+1][y+1])dfs(x + 1, y + 1, 1);
-		}
-	}
-}
+int map[17][17],N,table[17][17][3];
 int main(void)
 {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> N;
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
-	dfs(1, 2, 0);
-	cout << ans;
-	return 0;
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    table[1][2][0] = 1;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            cin >> map[i][j];
+            if (map[i][j])continue;
+            table[i][j][0] += table[i][j - 1][0] + table[i][j - 1][2];
+            table[i][j][1] += table[i - 1][j][1] + table[i - 1][j][2];
+            if (!map[i - 1][j] and !map[i][j - 1])table[i][j][2] += table[i - 1][j - 1][0] + table[i - 1][j - 1][1]+table[i-1][j-1][2];
+        }
+    }
+    cout << table[N][N][0] + table[N][N][1] + table[N][N][2];
+    return 0;
 }
