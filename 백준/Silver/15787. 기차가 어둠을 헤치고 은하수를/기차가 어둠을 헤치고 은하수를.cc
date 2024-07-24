78d9802fe1718662e8f3679bf28da0c7f97047dc
Woosung <127220113+ws19999@users.noreply.github.com>
2024-07-24 18:14:33 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 20 ms, Memory: 4108 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <deque>
 #include <unordered_map>
 using namespace std;
 int N, M,ans=0;
-deque<int> dq[100001];
+bool bitmasking[100001][20];
 int main(void)
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> N >> M;
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 0; j < 20; j++)dq[i].push_back(0);
-	}
+	unordered_map<int, int> check;
 	for (int o = 0; o < M; o++)
 	{
 		int d,i,x;
@@ -19,33 +15,34 @@ int main(void)
 		if (d == 1)
 		{
 			cin >> i >> x;
-			dq[i][x - 1] = 1;
+			int temp = 1;
+			bitmasking[i][x - 1] = true;
 		}
 		else if (d == 2)
 		{
 			cin >> i >> x;
-			dq[i][x - 1] = 0;
+			int temp = 1;
+			bitmasking[i][x - 1] = false;
 		}
 		else if (d == 3)
 		{
 			cin >> i;
-			dq[i].push_front(0);
-			dq[i].pop_back();
+			for (int a = 18; a >=0; a--)bitmasking[i][a + 1] = bitmasking[i][a];
+			bitmasking[i][0] = false;
 		}
 		else
 		{
 			cin >> i;
-			dq[i].pop_front();
-			dq[i].push_back(0);
+			for (int a = 0; a < 19; a++)bitmasking[i][a] = bitmasking[i][a+1];
+			bitmasking[i][19] = false;
 		}
 	}
-	unordered_map<int, int> check;
 	for (int i = 1; i <= N; i++)
 	{
 		int num = 0;
 		for (int j = 0; j < 20; j++)
 		{
-			num = num * 2 + dq[i][j];
+			num = num * 2 + bitmasking[i][j];
 		}
 		if (!check[num])
 		{
