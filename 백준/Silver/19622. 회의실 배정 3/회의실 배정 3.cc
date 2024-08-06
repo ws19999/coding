f79dbf00429bd4538bda7f504ba0961683e93207
Woosung <127220113+ws19999@users.noreply.github.com>
2024-08-06 15:11:55 +0900

    [Silver II] Title: 회의실 배정 3, Time: 44 ms, Memory: 8268 KB -BaekjoonHub

--- 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.cc
+++ 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <unordered_map>
-#include <algorithm>
 #include <vector>
 #include <tuple>
 using namespace std;
@@ -17,46 +16,11 @@ int main(void)
 		cin >> s >> e >> m;
 		meetings.push_back(make_tuple(s, e, m));
 	}
-	sort(meetings.begin(), meetings.end());
 	for (int i = 0; i < N; i++)
 	{
-		if (i == 0)
-		{
-			table[get<1>(meetings[i])] = get<2>(meetings[i]);
-		}
-		else if (i == 1)
-		{
-			if (get<0>(meetings[i]) < get<1>(meetings[i - 1]))
-			{
-				table[get<1>(meetings[i])] = get<2>(meetings[i]);
-			}
-			else
-			{
-				table[get<1>(meetings[i])] = get<2>(meetings[i]) + table[get<1>(meetings[i - 1])];
-			}
-		}
-		else if (i == 2)
-		{
-			if (get<0>(meetings[i]) < get<1>(meetings[i - 1]))
-			{
-				table[get<1>(meetings[i])] = get<2>(meetings[i])+ table[get<1>(meetings[i - 2])];
-			}
-			else
-			{
-				table[get<1>(meetings[i])] = get<2>(meetings[i]) + max(table[get<1>(meetings[i - 1])], table[get<1>(meetings[i - 2])]);
-			}
-		}
-		else
-		{
-			if (get<0>(meetings[i]) < get<1>(meetings[i - 1]))
-			{
-				table[get<1>(meetings[i])] = get<2>(meetings[i])+ max(table[get<1>(meetings[i - 3])], table[get<1>(meetings[i - 2])]);
-			}
-			else
-			{
-				table[get<1>(meetings[i])] = get<2>(meetings[i]) + max(table[get<1>(meetings[i - 1])], table[get<1>(meetings[i - 2])]);
-			}
-		}
+		if (i <= 1)table[get<1>(meetings[i])] = get<2>(meetings[i]);
+		else if (i == 2)table[get<1>(meetings[i])] = get<2>(meetings[i])+ table[get<1>(meetings[i - 2])];
+		else table[get<1>(meetings[i])] = get<2>(meetings[i])+ max(table[get<1>(meetings[i - 3])], table[get<1>(meetings[i - 2])]);
 		answer = max(answer, table[get<1>(meetings[i])]);
 	}
 	cout << answer;
