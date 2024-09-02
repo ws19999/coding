2e393e823ec3130ed5617f8fafffff3191c27017
Woosung <127220113+ws19999@users.noreply.github.com>
2024-09-02 10:53:45 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 76 ms, Memory: 12320 KB, Score: 64 point -BaekjoonHub

---
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.kt
@@ -0,0 +1,43 @@
+fun main()
+{
+    val br=System.`in`.bufferedReader()
+    val bw=System.`out`.bufferedWriter()
+    val N=br.readLine().toInt()
+    val S=br.readLine()
+    var answer=0
+    var pos=N-1
+    while(true)
+    {
+        if(pos<=1)
+        {
+            answer++
+            break
+        }
+        else if(pos==2)
+        {
+            if(S[pos-2]<='5' || (S[pos-2]=='6' && S[pos-1]<='3') || (S[pos-2]=='6' && S[pos-1]=='4' && S[pos]<='1'))
+            {
+                answer++
+                break
+            }
+            else
+            {
+                answer+=2
+                break
+            }
+        }
+        else
+        {
+            answer++
+            if(S[pos-2]=='0')
+            {
+                if(S[pos-1]=='0')pos-=1
+                else pos-=2
+            }
+            else if(S[pos-2]<='5' || (S[pos-2]=='6' && S[pos-1]<='3') || (S[pos-2]=='6' && S[pos-1]=='4' && S[pos]<='1'))pos-=3
+            else pos-=2
+        }
+    }
+    bw.write("${answer}")
+    bw.close()
+}
