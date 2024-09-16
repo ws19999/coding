71db92dc87421411f95d3895efe4ea82f9ab858d
Woosung <stardino99@gmail.com>
2024-09-16 10:34:55 +0900

    [Gold V] Title: 내려가기, Time: 2676 ms, Memory: 34272 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,8 +1,8 @@
 N=int(input())
-mins=[0,0,0]
-maxs=[0,0,0]
+최소=[0,0,0]
+최대=[0,0,0]
 for _ in range(N):
     arr=list(map(int,input().split(" ")))
-    mins[0],mins[1],mins[2]=min(mins[0],mins[1])+arr[0],min(mins[0],mins[1],mins[2])+arr[1],min(mins[1],mins[2])+arr[2]
-    maxs[0],maxs[1],maxs[2]=max(maxs[0],maxs[1])+arr[0],max(maxs[0],maxs[1],maxs[2])+arr[1],max(maxs[1],maxs[2])+arr[2]
-print(max(maxs),min(mins))
+    최소[0],최소[1],최소[2]=min(최소[0],최소[1])+arr[0],min(최소[0],최소[1],최소[2])+arr[1],min(최소[1],최소[2])+arr[2]
+    최대[0],최대[1],최대[2]=max(최대[0],최대[1])+arr[0],max(최대[0],최대[1],최대[2])+arr[1],max(최대[1],최대[2])+arr[2]
+print(max(최대),min(최소))
