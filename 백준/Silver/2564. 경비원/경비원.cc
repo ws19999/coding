f8bb9fb5b951a953a8a0c823bed3dfc193ce4bdc
Woosung <stardino99@gmail.com>
2024-09-27 10:41:21 +0900

    [Silver I] Title: 경비원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.cc
+++ 백준/Silver/2564. 경비원/경비원.cc
@@ -4,48 +4,23 @@ using namespace std;
 int main(void)
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	vector<pair<int, int>> vec;
-	int w, h;
-	cin >> w >> h;
-	int num;
-	cin >> num;
-	for (int i = 0; i < num; i++)
+	vector<pair<int, int>> block;
+	int w, h,answer=0,num;
+	cin >> w >> h >> num;
+	for (int i = 0; i <= num; i++)
 	{
 		int a, b;
 		cin >> a >> b;
-		if (a == 1)vec.push_back(make_pair(0, b));
-		else if (a == 2)vec.push_back(make_pair(h, b));
-		else if (a == 3)vec.push_back(make_pair(b, 0));
-		else vec.push_back(make_pair(b, w));
+		if (a == 1)block.push_back(make_pair(0, b));
+		else if (a == 2)block.push_back(make_pair(h, b));
+		else if (a == 3)block.push_back(make_pair(b, 0));
+		else block.push_back(make_pair(b, w));
 	}
-	pair<int,int> 동근;
-	int dir, dis;
-	cin >> dir >> dis;
-	if (dir == 1)
+	for (int i=0;i<num;i++)
 	{
-		동근.first = 0;
-		동근.second = dis;
-	}
-	else if (dir == 2)
-	{
-		동근.first = h;
-		동근.second = dis;
-	}
-	else if (dir == 3)
-	{
-		동근.first = dis;
-		동근.second = 0;
-	}
-	else {
-		동근.first = dis;
-		동근.second = w;
-	}
-	int answer = 0;
-	for (auto i : vec)
-	{
-		if (abs(i.first - 동근.first) == h) answer += h + min(i.second + 동근.second, w - i.second + w - 동근.second);
-		else if (abs(i.second - 동근.second) == w) answer += w + min(i.first + 동근.first, h - i.first + h - 동근.first);
-		else answer += abs(i.first - 동근.first) + abs(i.second - 동근.second);
+		if (abs(block[i].first - block[num].first) == h) answer += h + min(block[i].second + block[num].second, 2*w - block[i].second - block[num].second);
+		else if (abs(block[i].second - block[num].second) == w) answer += w + min(block[i].first + block[num].first, 2*h - block[i].first - block[num].first);
+		else answer += abs(block[i].first - block[num].first) + abs(block[i].second - block[num].second);
 	}
 	cout << answer;
 	return 0;
