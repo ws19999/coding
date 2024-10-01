a96cc839c13fc2166215329fe17fd4c2fd789629
Woosung <stardino99@gmail.com>
2024-10-02 08:57:46 +0900

    [Silver I] Title: 내려가기 2, Time: 156 ms, Memory: 110656 KB -BaekjoonHub

---
+++ 백준/Silver/15645. 내려가기 2/내려가기 2.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+N=int(input())
+최소=[0,0,0]
+최대=[0,0,0]
+for _ in range(N):
+    arr=list(map(int,input().split(" ")))
+    최소[0],최소[1],최소[2]=min(최소[:2])+arr[0],min(최소)+arr[1],min(최소[1:])+arr[2]
+    최대[0],최대[1],최대[2]=max(최대[:2])+arr[0],max(최대)+arr[1],max(최대[1:])+arr[2]
+print(max(최대),min(최소))
