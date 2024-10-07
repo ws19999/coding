605b418fc5de7282997ef264ad1dd73b32b54563
Woosung <stardino99@gmail.com>
2024-10-08 08:19:21 +0900

    [Bronze II] Title: 보물 지도, Time: 436 ms, Memory: 56604 KB -BaekjoonHub

---
+++ 백준/Bronze/29332. 보물 지도/보물 지도.kt
@@ -0,0 +1,24 @@
+import kotlin.math.max
+import kotlin.math.min
+import java.math.BigInteger
+fun main()
+{
+    val br=System.`in`.bufferedReader()
+    val bw=System.`out`.bufferedWriter()
+    val N=br.readLine().toInt()
+    var maxx=1000000001
+    var minx=-1000000001
+    var maxy = 1000000001
+    var miny = -1000000001
+    for (i in 1..N)
+    {
+        var (x,y,d)=br.readLine().trim().split(" ")
+        if(d=="R") minx=max(minx,x.toInt())
+        else if(d=="L")maxx=min(maxx,x.toInt())
+        else if(d=="U")miny=max(miny,y.toInt())
+        else maxy=min(maxy,y.toInt())
+    }
+    if(maxx==1000000001 || maxy==1000000001 || minx == -1000000001 || miny == -1000000001)bw.write("Infinity")
+    else bw.write("${((maxx-minx-1).toBigInteger())*((maxy-miny-1).toBigInteger())}")
+    bw.close()
+}
