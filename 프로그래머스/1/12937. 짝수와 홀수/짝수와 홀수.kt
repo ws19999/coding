b38246b53e4402d1ddb8bc4e8337990e29cdff4c
Woosung <stardino99@gmail.com>
2024-11-25 19:06:15 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
@@ -1,8 +1,11 @@
 class Solution {
     fun solution(num: Int): String {
         var answer = ""
-        if(num%2==0)answer="Even"
-        else answer="Odd"
+        when (num%2)
+        {
+           0 -> answer="Even"
+           else -> answer="Odd"
+        }
         return answer
     }
 }
