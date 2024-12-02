c4450db96cf9c1ae717f5c561070a2c8c92fef38
Woosung <stardino99@gmail.com>
2024-12-02 11:35:17 +0900

    [Silver IV] Title: 디지털 친구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1985. 디지털 친구/디지털 친구.cc
+++ 백준/Silver/1985. 디지털 친구/디지털 친구.cc
@@ -1,5 +1,57 @@
 #include <iostream>
 using namespace std;
+bool check(string x, string y,int nums1[10],int nums2[10])
+{
+	bool almost_friends=false;
+	for (int i = 0; i < x.length() - 1; i++)
+	{
+		if (x[i] != '0' and (i != 0 or x[i] != '1') and x[i + 1] != '9')
+		{
+			nums1[x[i] - '0']--;
+			nums1[x[i + 1] - '0']--;
+			nums1[x[i] - 1 - '0']++;
+			nums1[x[i + 1] + 1 - '0']++;
+			almost_friends = true;
+			for (int k = 0; k < 10; k++)
+			{
+				if ((nums1[k] and nums2[k]) or (!nums1[k] and !nums2[k]))continue;
+				else
+				{
+					almost_friends = false;
+					break;
+				}
+			}
+			nums1[x[i] - '0']++;
+			nums1[x[i + 1] - '0']++;
+			nums1[x[i] - 1 - '0']--;
+			nums1[x[i + 1] + 1 - '0']--;
+			if (almost_friends)return true;
+		}
+		if (x[i] != '9' and x[i + 1] != '0')
+		{
+			nums1[x[i] - '0']--;
+			nums1[x[i + 1] - '0']--;
+			nums1[x[i] + 1 - '0']++;
+			nums1[x[i + 1] - 1 - '0']++;
+			almost_friends = true;
+			for (int k = 0; k < 10; k++)
+			{
+				if ((nums1[k] and nums2[k]) or (!nums1[k] and !nums2[k]))continue;
+				else
+				{
+					almost_friends = false;
+					break;
+				}
+			}
+			nums1[x[i] - '0']++;
+			nums1[x[i + 1] - '0']++;
+			nums1[x[i] + 1 - '0']--;
+			nums1[x[i + 1] - 1 - '0']--;
+			if (almost_friends)return true;
+		}
+	}
+	return false;
+}
 int main(void)
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -8,7 +60,7 @@ int main(void)
 		string x, y;
 		cin >> x >> y;
 		bool friends = true;
-		bool almost_friends = false;
+		bool almost_friends;
 		int nums1[10] = { 0 };
 		int nums2[10] = { 0 };
 		for (auto i : x)nums1[i - '0']++;
@@ -25,105 +77,8 @@ int main(void)
 		if (friends)cout << "friends\n";
 		else
 		{
-			for (int i = 0; i < x.length()-1; i++)
-			{
-				if (x[i] != '0' and (i!=0 or x[i]!='1') and x[i+1]!='9')
-				{
-					nums1[x[i] - '0']--;
-					nums1[x[i + 1] - '0']--;
-					nums1[x[i] - 1 - '0']++;
-					nums1[x[i + 1]+1 - '0']++;
-					almost_friends = true;
-					for (int k = 0; k < 10; k++)
-					{
-						if ((nums1[k] and nums2[k]) or (!nums1[k] and !nums2[k]))continue;
-						else
-						{
-							almost_friends = false;
-							break;
-						}
-					}
-					nums1[x[i] - '0']++;
-					nums1[x[i + 1] - '0']++;
-					nums1[x[i] - 1 - '0']--;
-					nums1[x[i + 1] + 1 - '0']--;
-					if (almost_friends)break;
-				}
-				if (x[i] != '9' and x[i + 1] != '0')
-				{
-					nums1[x[i] - '0']--;
-					nums1[x[i + 1] - '0']--;
-					nums1[x[i] + 1 - '0']++;
-					nums1[x[i + 1] - 1 - '0']++;
-					almost_friends = true;
-					for (int k = 0; k < 10; k++)
-					{
-						if ((nums1[k] and nums2[k]) or (!nums1[k] and !nums2[k]))continue;
-						else
-						{
-							almost_friends = false;
-							break;
-						}
-					}
-					nums1[x[i] - '0']++;
-					nums1[x[i + 1] - '0']++;
-					nums1[x[i] + 1 - '0']--;
-					nums1[x[i + 1] - 1 - '0']--;
-					if (almost_friends)break;
-				}
-			}
-			if (almost_friends)
-			{
-				cout << "almost friends\n";
-				continue;
-			}
-			for (int i = 0; i < y.length()-1; i++)
-			{
-				if (y[i] != '0' and (i != 0 or y[i] != '1') and y[i + 1] != '9')
-				{
-					nums2[y[i] - '0']--;
-					nums2[y[i + 1] - '0']--;
-					nums2[y[i] - 1 - '0']++;
-					nums2[y[i + 1] + 1 - '0']++;
-					almost_friends = true;
-					for (int k = 0; k < 10; k++)
-					{
-						if ((nums1[k] and nums2[k]) or (!nums1[k] and !nums2[k]))continue;
-						else
-						{
-							almost_friends = false;
-							break;
-						}
-					}
-					nums2[y[i] - '0']++;
-					nums2[y[i + 1] - '0']++;
-					nums2[y[i] - 1 - '0']--;
-					nums2[y[i + 1] + 1 - '0']--;
-					if (almost_friends)break;
-				}
-				if (y[i] != '9' and y[i + 1] != '0')
-				{
-					nums2[y[i] - '0']--;
-					nums2[y[i + 1] - '0']--;
-					nums2[y[i] + 1 - '0']++;
-					nums2[y[i + 1] - 1 - '0']++;
-					almost_friends = true;
-					for (int k = 0; k < 10; k++)
-					{
-						if ((nums1[k] and nums2[k]) or (!nums1[k] and !nums2[k]))continue;
-						else
-						{
-							almost_friends = false;
-							break;
-						}
-					}
-					nums2[y[i] - '0']++;
-					nums2[y[i + 1] - '0']++;
-					nums2[y[i] + 1 - '0']--;
-					nums2[y[i + 1] - 1 - '0']--;
-					if (almost_friends)break;
-				}
-			}
+			almost_friends=check(x, y, nums1, nums2);
+			if (!almost_friends)almost_friends = check(y, x, nums2, nums1);
 			if (almost_friends)cout << "almost friends\n";
 			else cout << "nothing\n";
 		}
