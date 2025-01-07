42444606e18f0caf348bba7023bb4c7e09113e45
Woosung <stardino99@gmail.com>
2025-01-07 10:25:46 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 20 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -1,94 +1,52 @@
 #include <iostream>
-#include <climits>
-#include <deque>
-#include <tuple>
+#include <unordered_map>
 using namespace std;
-int ans = INT_MAX;
-int table[100002][5][5];
-int dir[100002];
-int main(void)
-{
+int table[2][11];
+unordered_map<int, pair<int,int>> pos;
+unordered_map<int, int> nextpos;
+void writescore(int tc, int nextj, int j, int score) {
+	if (!table[(tc + 1) % 2][nextj])table[(tc + 1) % 2][nextj] = table[tc % 2][j] + score;
+	else table[(tc + 1) % 2][nextj] = min(table[(tc + 1) % 2][nextj], table[tc % 2][j] + score);
+}
+void check(int tc,int p,int i,int j,int dir) {
+	int nextj = nextpos[min(dir, j) * 5 + max(dir, j)];
+	if (i == 0) writescore(tc, nextj, p, 2);
+	else if (abs(dir - i) == 2) writescore(tc, nextj, p, 4);
+	else writescore(tc, nextj, p, 3);
+}
+int main(void) {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int i = 1;
-	dir[0] = 1;
-	while(dir[i-1])
-	{
-		cin >> dir[i];
-		for (int a = 0; a < 5; a++)
-		{
-			for (int b = 0; b < 5; b++)
-			{
-				table[i][a][b] = INT_MAX;
+	int p = 1,a=0,b=0,tc=0,dir;
+	for (int i = 0; i < 4; i++) {
+		for (int j = i + 1; j <= 4; j++) {
+			pos[p] = make_pair(i, j);
+			nextpos[i * 5 + j] = p++;
+		}
+	}
+	while (true) {
+		cin >> dir;
+		if (dir == 0)break;
+		for (int i = 1; i <= 10; i++)table[(tc+1) % 2][i] = 0;
+		if (tc == 0)table[1][dir] = 2;
+		else {
+			for (int j = 1; j <= 10; j++) {
+				if (table[tc%2][j]) {
+					a = pos[j].first;
+					b = pos[j].second;
+					if (a == dir or b == dir) writescore(tc, j, j, 1);
+					else {
+						check(tc, j,a, b,dir);
+						check(tc, j,b, a,dir);
+					}
+				}
 			}
 		}
-		i++;
+		tc++;
 	}
-	deque<tuple<int, int, int>> dq;
-	dq.push_back(make_tuple(1, 0, 0));
-	while (!dq.empty())
-	{
-		int num = get<0>(dq.front());
-		int left = get<1>(dq.front());
-		int right = get<2>(dq.front());
-		dq.pop_front();
-		if (dir[num] == 0)continue;
-		if (left == dir[num] && table[num - 1][left][right] + 1 < table[num][dir[num]][right] && dir[num] != right)
-		{
-			table[num][dir[num]][right] = table[num - 1][left][right] + 1;
-			table[num][right][dir[num]] = table[num - 1][left][right] + 1;
-			dq.push_back(make_tuple(num + 1, dir[num], right));
-		}
-		else if (left == 0 && table[num - 1][left][right] + 2 < table[num][dir[num]][right] && dir[num] != right)
-		{
-			table[num][dir[num]][right] = table[num - 1][left][right] + 2;
-			table[num][right][dir[num]] = table[num - 1][left][right] + 2;
-			dq.push_back(make_tuple(num + 1, dir[num], right));
-		}
-		else if (abs(left - dir[num]) != 2 && table[num - 1][left][right] + 3 < table[num][dir[num]][right] && dir[num] != right)
-		{
-			table[num][dir[num]][right] = table[num - 1][left][right] + 3;
-			table[num][right][dir[num]] = table[num - 1][left][right] + 3;
-			dq.push_back(make_tuple(num + 1, dir[num], right));
-		}
-		else if (table[num - 1][left][right] + 4 < table[num][dir[num]][right] && dir[num] != right)
-		{
-			table[num][dir[num]][right] = table[num - 1][left][right] + 4;
-			table[num][right][dir[num]] = table[num - 1][left][right] + 4;
-			dq.push_back(make_tuple(num + 1, dir[num], right));
-		}
-		if (right == dir[num] && table[num - 1][left][right] + 1 < table[num][left][dir[num]] && dir[num] != left)
-		{
-			table[num][left][dir[num]] = table[num - 1][left][right] + 1;
-			table[num][dir[num]][left] = table[num - 1][left][right] + 1;
-			dq.push_back(make_tuple(num + 1, left,dir[num]));
-		}
-		else if (right == 0 && table[num - 1][left][right] + 2 < table[num][left][dir[num]] && dir[num] != left)
-		{
-			table[num][left][dir[num]] = table[num - 1][left][right] + 2;
-			table[num][dir[num]][left] = table[num - 1][left][right] + 2;
-			dq.push_back(make_tuple(num + 1, left, dir[num]));
-		}
-		else if (abs(right - dir[num]) != 2 && table[num - 1][left][right] + 3 < table[num][left][dir[num]] && dir[num] != left)
-		{
-			table[num][left][dir[num]] = table[num - 1][left][right] + 3;
-			table[num][dir[num]][left] = table[num - 1][left][right] + 3;
-			dq.push_back(make_tuple(num + 1, left, dir[num]));
-		}
-		else if (table[num - 1][left][right] + 4 < table[num][left][dir[num]] && dir[num] != left)
-		{
-			table[num][left][dir[num]] = table[num - 1][left][right] + 4;
-			table[num][dir[num]][left] = table[num - 1][left][right] + 4;
-			dq.push_back(make_tuple(num + 1, left, dir[num]));
-		}
+	int answer = 4 * tc;
+	for (int i = 1; i <= 10; i++) {
+		if (table[tc % 2][i])answer = min(answer, table[tc % 2][i]);
 	}
-	int ans = INT_MAX;
-	for (int a = 0; a < 5; a++)
-	{
-		for (int b = a+1; b < 5; b++)
-		{
-			ans = min(ans, table[i - 2][a][b]);
-		}
-	}
-	cout << ans;
+	cout << answer;
 	return 0;
 }
