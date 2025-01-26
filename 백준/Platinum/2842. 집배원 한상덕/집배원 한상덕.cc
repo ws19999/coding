30976884f675caf7dac287e63eb1affc29dd3871
Woosung <stardino99@gmail.com>
2025-01-26 19:33:22 +0900

    [Platinum IV] Title: 집배원 한상덕, Time: 784 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.cc
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.cc
@@ -7,10 +7,9 @@ int height[50][50];
 int dx[] = { -1,-1,-1,0,1,1,1,0 }, dy[] = { -1,0,1,1,1,0,-1,-1 };
 char village[50][50];
 int housecnt = 0;
-int startx, starty,N,minnmax=0,maxxmin=1000000,ans=1000000;
+int startx, starty,N,minnmax=0,ans=1000000;
 deque<int> highs;
 bool startmorning(int minh, int maxh) {
-    if (height[startx][starty] < minh)return false;
     bool visited[50][50] = { false };
     int cnt=0;
     deque<pair<int, int>> dq;
@@ -38,10 +37,12 @@ bool startmorning(int minh, int maxh) {
     }
     return false;
 }
-void bsearch(int l, int r,int maxh) { //높이차
+void bsearch(int l, int r) { //최대높이는 highs[r]일때 최소높이 찾기.
+    int maxh = highs[r];
     while (l <= r) {
         int m = (l + r) / 2;
-        if (startmorning(highs[m], maxh)) { //높이차를 더 줄여야댐.
+        if (height[startx][starty] < highs[m])r = m - 1; //최소 높이 낮춤
+        else if (startmorning(highs[m], maxh)) { //범위 안이 가능하므로 최소높이 올리기.
             ans = min(ans, maxh-highs[m]);
             l = m + 1;
         }
@@ -70,16 +71,13 @@ int main(void)
                 keys[height[i][j]] = 1;
                 highs.push_back(height[i][j]);
             }
-            if (village[i][j] != '.') {
-                minnmax = max(minnmax, height[i][j]);
-                maxxmin = min(maxxmin, height[i][j]);
-            }
+            if (village[i][j] != '.')minnmax = max(minnmax, height[i][j]);
         }
     }
     sort(highs.begin(), highs.end());
     for (int i = highs.size() - 1; i >= 0; i--) {
-        if (highs[i] < minnmax )break;
-        bsearch(0, i, highs[i]); //높이차. 
+        if (highs[i] < minnmax)break;
+        bsearch(0, i); 
     }
     cout << ans;
     return 0;
