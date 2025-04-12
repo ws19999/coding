9d63a50d5935bbcbdaa01754ccd60dcb1fff81fb
Woosung <stardino99@gmail.com>
2025-04-13 08:35:22 +0900

    [Bronze II] Title: 방 배정, Time: 112 ms, Memory: 21320 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.kt
@@ -0,0 +1,20 @@
+import kotlin.math.*
+fun main()
+{
+    val br=System.`in`.bufferedReader()
+    val bw=System.`out`.bufferedWriter()
+    val (N,K)=br.readLine().split(" ").map{it.toInt()}
+    var arr = Array(7){IntArray(2){0}}
+    repeat(N){
+        val(S,Y)=br.readLine().split(" ").map{it.toInt()}
+        arr[Y][S]++
+    }
+    var ans=0
+    for(i in 1..6){
+        for(j in 0..1){
+            ans+=arr[i][j]/2 + arr[i][j]%2
+        }
+    }
+    bw.write("$ans")
+    bw.close()
+}
