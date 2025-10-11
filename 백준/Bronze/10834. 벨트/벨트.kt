803b209823bc488e8402e51fdd351e80e5e330ff
Woosung <stardino99@gmail.com>
2025-10-11 11:06:55 +0900

    [Bronze II] Title: 벨트, Time: 152 ms, Memory: 22612 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.kt
+++ 백준/Bronze/10834. 벨트/벨트.kt
@@ -1,10 +1,10 @@
 fun main(){
     val m=readln().toInt()
-    var temp=1
+    var temp=1L
     var flag=0
     repeat(m){
-        var (a,b,s)=readln().split(" ").map { it.toInt() }
-        if(s==1){
+        val (a,b,s)=readln().split(" ").map { it.toLong() }
+        if(s==1L){
             flag=1-flag
         }
         if(temp>=a){
