427a22e3650f0b594facec508ad3ece0e70ff0f8
Woosung <stardino99@gmail.com>
2025-10-12 13:52:12 +0900

    [Bronze I] Title: Assigning Partners, Time: 108 ms, Memory: 20800 KB -BaekjoonHub

--- 백준/Bronze/10105. Assigning Partners/Assigning Partners.kt
+++ 백준/Bronze/10105. Assigning Partners/Assigning Partners.kt
@@ -3,20 +3,24 @@ fun main(){
     val n=readln().toInt()
     val str1=readln().trim().split(" ")
     val str2=readln().trim().split(" ")
-
+    if(n%2==1){
+        println("bad")
+        return
+    }
     for(i in 0 until n){
-        val person1 = str1[i]
-        val person2 = str2[i]
-        if (person1 == person2) {
+        if(str1[i]==str2[i]){
             println("bad")
             return
         }
-        partners[person1] = person2
-    }
-    for ((person, partner) in partners) {
-        if (partners[partner] != person) {
-            println("bad")
-            return
+        if(partners.containsKey(str1[i])){
+            if(partners[str1[i]]!=str2[i]){
+                println("bad")
+                return
+            }
+        }
+        else{
+            partners.put(str1[i],str2[i])
+            partners.put(str2[i],str1[i])
         }
     }
     println("good")
