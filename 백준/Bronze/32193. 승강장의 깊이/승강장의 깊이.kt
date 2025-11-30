f03bb5f84e9bbbb2f03824881976314f0fe8bb37
Woosung <stardino99@gmail.com>
2025-11-30 14:15:42 +0900

    [Bronze III] Title: 승강장의 깊이, Time: 656 ms, Memory: 115852 KB -BaekjoonHub

--- 백준/Bronze/32193. 승강장의 깊이/승강장의 깊이.kt
+++ 백준/Bronze/32193. 승강장의 깊이/승강장의 깊이.kt
@@ -1,11 +1,14 @@
 fun main(){
-    val n=readln().toInt()
+    val br=System.`in`.bufferedReader()
+    val bw=System.`out`.bufferedWriter()
+    val n=br.readLine().toInt()
     var surf=0
     var platform=0
     repeat(n){
-        val (a,b)=readln().split(" ").map { it.toInt() }
+        val (a,b)=br.readLine().split(" ").map { it.toInt() }
         surf+=a
         platform+=b
-        println(surf-platform)
+        bw.write("${surf-platform}\n")
     }
+    bw.close()
 }
