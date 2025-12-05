8303525c223e4ef5f07f5bca459e6005b0d4fe52
Woosung <stardino99@gmail.com>
2025-12-06 08:54:22 +0900

    [Bronze I] Title: Book Club, Time: 600 ms, Memory: 110668 KB -BaekjoonHub

--- 백준/Bronze/5947. Book Club/Book Club.kt
+++ 백준/Bronze/5947. Book Club/Book Club.kt
@@ -1,13 +1,16 @@
 fun main(){
-    val (n,nq,p)=readln().split(" ").map{it.toInt()}
+    val br=System.`in`.bufferedReader()
+    val bw=System.`out`.bufferedWriter()
+    val (n,nq,p)=br.readLine().split(" ").map{it.toInt()}
     val arr = Array(n){IntArray(nq){0} }
     for(i in 0..<n){
-        arr[i]=readln().split(" ").map { it.toInt() }.toIntArray()
+        arr[i]=br.readLine().split(" ").map { it.toInt() }.toIntArray()
     }
     val ans = (0..<n).toMutableSet() // 0부터 n-1까지의 정수로 MutableSet 초기화
     repeat(p){
-        val(a,b)=readln().split(" ").map { it.toInt() }
+        val(a,b)=br.readLine().split(" ").map { it.toInt() }
         ans.retainAll { i -> arr[i][a-1] == b }
     }
-    println(ans.size)
+    bw.write("${ans.size}")
+    bw.close()
 }
