98c3fbccb94c6f4f3d45d98afcfe2ac16de96c34
Woosung <stardino99@gmail.com>
2026-02-06 11:46:13 +0900

    [Bronze III] Title: Isn’t It Funny How a Bear Likes Honey?, Time: 128 ms, Memory: 22544 KB -BaekjoonHub

--- 백준/Bronze/5163. Isn’t It Funny How a Bear Likes Honey？/Isn’t It Funny How a Bear Likes Honey？.kt
+++ 백준/Bronze/5163. Isn’t It Funny How a Bear Likes Honey？/Isn’t It Funny How a Bear Likes Honey？.kt
@@ -1,10 +1,10 @@
 import kotlin.math.PI
 
 fun main(){
-    val k=readln().trim().toInt()
+    val k=readln().toInt()
     for(sets in 1..k){
         println("Data Set $sets:")
-        var (b,w)=readln().trim().split(" ").map{it.toDouble()}
+        var (b,w)=readln().split(" ").map{it.toDouble()}
         var limit=0.0
         w*=1000
         repeat(b.toInt()){
